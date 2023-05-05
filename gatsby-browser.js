ee7806b129f628ed31d980ba350545141965d11b
ayushthe1 <ayushsharmaa101@gmail.com>
2023-05-05 11:46:55 +0530

    move script ot gatsby-browser
    Signed-off-by: ayushthe1 <ayushsharmaa101@gmail.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,38 @@
 import "./fonts.css";
 
+const addGTMToHead = () => {
+  if (window.gtmDidInit) {
+    return false;
+  }
+  window.gtmDidInit = true;
+  const script = document.createElement("script");
+  script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W92XDVZ');`;
+  script.async = true;
+
+  document.head.appendChild(script);
+};
+
+const addGTMToBody = () => {
+  const noscript = document.createElement("noscript");
+  const iframe = document.createElement("iframe");
+  iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-W92XDVZ";
+  iframe.height = 0;
+  iframe.width = 0;
+  iframe.style.display = "none";
+  iframe.style.visibility = "hidden";
+  noscript.appendChild(iframe);
+
+  document.body.insertBefore(noscript, document.body.firstChild);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  addGTMToHead();
+  addGTMToBody();
+});
+
 export { wrapRootElement } from "./root-wrapper";
 export { wrapPageElement } from "./page-wrapper";
